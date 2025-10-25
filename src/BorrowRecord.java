942fbb228be6d60039551a9875627b1c17fc1ce8
HesamZandy <Zndyhsam@gmail.com>
2025-10-25 13:11:41 +0330

    Add set & get for BorrowRecord Class

--- src/BorrowRecord.java
+++ src/BorrowRecord.java
@@ -17,4 +17,39 @@ public class BorrowRecord {
         this.borrowDate = borrowDate;
         this.dueDate = dueDate;
     }
+
+    //setter & getter for return and set attributes
+
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public LocalDate getBorrowDate() {
+        return borrowDate;
+    }
+
+    public void setBorrowDate(LocalDate borrowDate) {
+        this.borrowDate = borrowDate;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
 }
