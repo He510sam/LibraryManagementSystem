a3a31882cdc174ef8bdb90702b965b59455f8fb0
HesamZandy <Zndyhsam@gmail.com>
2025-10-25 12:51:58 +0330

    Add attributes for book Class

--- src/Book.java
+++ src/Book.java
@@ -8,12 +8,56 @@ public class Book {
     private String title;
     private String author;
     private boolean isAvailable;
+    private int copies;
 
     //constructor for Initializing fields
-    public Book(int bookId, String title, String author) {
+    public Book(int bookId, String title, String author, int copies) {
         this.bookId = bookId;
         this.title = title;
         this.author = author;
         this.isAvailable = true;
+        this.copies = copies;
+    }
+
+    //setter & getter for return and set specific attributes
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    public int getCopies() {
+        return copies;
+    }
+
+    public void setCopies(int copies) {
+        this.copies = copies;
     }
 }
