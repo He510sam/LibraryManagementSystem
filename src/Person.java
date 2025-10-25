4e6c03b0c584dc75960387a5b490b7eeb6a42772
HesamZandy <Zndyhsam@gmail.com>
2025-10-25 11:21:04 +0330

    Add id , name & address attribute to Person class

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,10 @@
 public abstract class Person {
+    private int id;
+    private String name;
+    private String address;
+    public Person(int id, String name, String address){
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
 }
