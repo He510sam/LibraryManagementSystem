a0edb0229e47d19cc19d7c4dcdd045b7c7a6328c
HesamZandy <Zndyhsam@gmail.com>
2025-10-25 11:33:04 +0330

    Add setter and getter for attributes in Person class and method for info

--- src/Person.java
+++ src/Person.java
@@ -1,10 +1,47 @@
+//Abstract class for everyone in the system
 public abstract class Person {
+    //add attributes
+
     private int id;
     private String name;
     private String address;
+    //constructor for Initializing fields
+
     public Person(int id, String name, String address){
         this.id = id;
         this.name = name;
         this.address = address;
+    }
+    //setter & getter for return and set attributes
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    //to show persons info
+    //its override because it has more options in child classes
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Name: " + name + ", Address: " + address;
     }
 }
